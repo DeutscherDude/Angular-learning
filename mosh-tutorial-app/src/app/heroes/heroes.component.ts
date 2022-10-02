eb145003909dfa063c6e3639bebc07f8066cdf56
Jakub <jakub.barczewski@netguru.com>
2022-10-02 20:09:30 +0200

    Almost done with lesson 5

--- mosh-tutorial-app/src/app/heroes/heroes.component.ts
+++ mosh-tutorial-app/src/app/heroes/heroes.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
 import { Hero } from './hero';
 
 @Component({
@@ -10,22 +9,15 @@ import { Hero } from './hero';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
-  constructor(
-    private readonly heroService: HeroService,
-    private readonly messageService: MessageService
-  ) {}
 
-  selectedHero?: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=$(hero.id)`);
-  }
-
-  getHeroes(): void {
-    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
-  }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
     this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
   }
 }
